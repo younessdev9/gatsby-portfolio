80caa176d4b28978dfadc7b3f635c113bbc1da49
younessdev9 <hassani.youness1999@gmail.com>
2020-09-13 09:41:51 +0100

    modifying the thank-you page

--- src/pages-styles/thank.js
+++ src/pages-styles/thank.js
@@ -8,16 +8,19 @@ const StyledThank = styled.div`
   justify-content: center;
 
   .success-logo {
-    max-width: 15rem;
-    max-height: 15rem;
+    max-width: 18rem;
+    max-height: 18rem;
   }
 
   h3 {
     font-weight: normal;
     color: #00b7c2;
     font-size: 2rem;
-    max-width: 90%;
+    max-width: 45%;
     text-align: center;
+    letter-spacing: 1px;
+    line-height: 1.7;
+    margin-top: 3rem;
     a {
       color: #2a9d8f;
       font-weight: 500;
