8fd807d91f26187066607259b7957ce8c529a086
younessdev9 <hassani.youness1999@gmail.com>
2020-09-23 22:55:50 +0100

    adding animation to side bar menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import DarkModeToggle from "react-dark-mode-toggle"
 import { StyledMenu } from "./menu.styled"
+import Fade from "react-reveal/Fade"
 const Menu = ({ open, setOpen, toggle, checked }) => {
   return (
     <StyledMenu open={open} onClick={() => setOpen(!open)}>
@@ -14,18 +15,20 @@ const Menu = ({ open, setOpen, toggle, checked }) => {
         />
       </div>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+        <Fade right cascade>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </Fade>
       </ul>
     </StyledMenu>
   )
