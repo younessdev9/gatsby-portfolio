33c1f32fb1c80e9f3c0d5b73ca2c44996b3ba5da
younessdev9 <hassani.youness1999@gmail.com>
2020-12-03 13:01:36 +0100

    fixing window issue on build

--- src/components/useTheme.js
+++ src/components/useTheme.js
@@ -2,32 +2,36 @@ import { useState, useEffect } from "react"
 
 function useTheme() {
   const currentTheme = () => {
-    let currentTheme = window.localStorage.getItem("theme") || "light"
-    if (currentTheme) {
+    if (typeof window !== `undefined`) {
+      let currentTheme = window.localStorage.getItem("theme") || "light"
+      if (currentTheme) {
+        return currentTheme
+      } else {
+        window.localStorage.setItem("theme", "light")
+      }
       return currentTheme
-    } else {
-      window.localStorage.setItem("theme", "light")
     }
-    return currentTheme
   }
   const [theme, setTheme] = useState(currentTheme)
   const toggleTheme = () => {
-    if (theme === "light") {
+    if (theme === "light" && typeof window !== `undefined`) {
       window.localStorage.setItem("theme", "dark")
       setTheme("dark")
-    } else {
+    } else if (typeof window !== `undefined`) {
       window.localStorage.setItem("theme", "light")
       setTheme("light")
     }
   }
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme")
-    if (localTheme) {
-      setTheme(localTheme)
-    } else {
-      setTheme("light")
-      window.localStorage.setItem("theme", "light")
+    if (typeof window !== `undefined`) {
+      const localTheme = window.localStorage.getItem("theme")
+      if (localTheme) {
+        setTheme(localTheme)
+      } else {
+        setTheme("light")
+        window.localStorage.setItem("theme", "light")
+      }
     }
   }, [theme])
   const handleChecking = () => {
